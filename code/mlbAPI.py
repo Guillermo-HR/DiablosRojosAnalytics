e8be1457a6769e62bc1f3b1550e1fa35ba5dca70
Guillermo <guillermo.hre@gmail.com>
2025-06-25 13:17:37 -0600

    Refactor data extraction in mlbAPI.py to include Batters data and update Pitchers data structure
    - Updated getPitchers function to correctly capture BattersFaced and TotalOuts.
    - Changed the logic to filter players based on position and stats availability.
    - Added getBatters function to extract batting statistics and save to CSV.
    - Adjusted CSV output paths for consistency.
    - Updated diabos.ipynb to incorporate new data processing steps for pitchers and batters.

--- code/mlbAPI.py
+++ code/mlbAPI.py
@@ -8,7 +8,8 @@ def getPitchers(rawData):
     df = {
         'Team': [],
         'Pitcher': [],
-        'Outs': [],
+        'BattersFaced': [],
+        'TotalOuts': [],
         'Singles': [],
         'Doubles': [],
         'Triples': [],
@@ -22,44 +23,109 @@ def getPitchers(rawData):
     }
     data = rawData['liveData']['boxscore']
     away = data['teams']['away']['team']['name']
-    for jugador in data['teams']['away']['players']:
-        if jugador['position']['code'] == '1' and len(jugador['stats']['pitching']) > 0:
-            df['Team'].append(away)
-            df['Pitcher'].append(jugador['person']['fullName'])
-            df['Outs'].append(jugador['stats']['pitching']['outs'])
-            df['Doubles'].append(jugador['stats']['pitching']['doubles'])
-            df['Triples'].append(jugador['stats']['pitching']['triples'])
-            df['HomeRuns'].append(jugador['stats']['pitching']['homeRuns'])
-            df['Singles'].append(jugador['stats']['pitching']['hits'] - df['Doubles'][-1] - df['Triples'][-1] - df['HomeRuns'][-1])
-            df['Walks'].append(jugador['stats']['pitching']['baseOnBalls'] + jugador['stats']['pitching']['intentionalWalks'])
-            df['HP'].append(jugador['stats']['pitching']['hitByPitch'])
-            df['atBats'].append(jugador['stats']['pitching']['atBats'])
-            df['runs'].append(jugador['stats']['pitching']['runs'])
-            df['earnedRuns'].append(jugador['stats']['pitching']['earnedRuns'])
-            df['seasonERA'].append(float(jugador['seasonStats']['pitching']['era']))
+    for jugador in data['teams']['away']['players'].values():
+        if jugador['position']['code'] != '1' or len(jugador['stats']['pitching']) == 0:
+            continue
+        df['Team'].append(away)
+        df['Pitcher'].append(jugador['person']['fullName'])
+        df['BattersFaced'].append(jugador['stats']['pitching']['battersFaced'])
+        df['TotalOuts'].append(jugador['stats']['pitching']['outs'])
+        df['Doubles'].append(jugador['stats']['pitching']['doubles'])
+        df['Triples'].append(jugador['stats']['pitching']['triples'])
+        df['HomeRuns'].append(jugador['stats']['pitching']['homeRuns'])
+        df['Singles'].append(jugador['stats']['pitching']['hits'] - df['Doubles'][-1] - df['Triples'][-1] - df['HomeRuns'][-1])
+        df['Walks'].append(jugador['stats']['pitching']['baseOnBalls'] + jugador['stats']['pitching']['intentionalWalks'])
+        df['HP'].append(jugador['stats']['pitching']['hitByPitch'])
+        df['atBats'].append(jugador['stats']['pitching']['atBats'])
+        df['runs'].append(jugador['stats']['pitching']['runs'])
+        df['earnedRuns'].append(jugador['stats']['pitching']['earnedRuns'])
+        df['seasonERA'].append(float(jugador['seasonStats']['pitching']['era']))
 
     home = data['teams']['home']['team']['name']
-    for jugador in data['teams']['home']['players']:
-        if jugador['position']['code'] == '1' and len(jugador['stats']['pitching']) > 0:
-            df['Team'].append(home)
-            df['Pitcher'].append(jugador['person']['fullName'])
-            df['Outs'].append(jugador['stats']['pitching']['outs'])
-            df['Doubles'].append(jugador['stats']['pitching']['doubles'])
-            df['Triples'].append(jugador['stats']['pitching']['triples'])
-            df['HomeRuns'].append(jugador['stats']['pitching']['homeRuns'])
-            df['Singles'].append(jugador['stats']['pitching']['hits'] - df['Doubles'][-1] - df['Triples'][-1] - df['HomeRuns'][-1])
-            df['Walks'].append(jugador['stats']['pitching']['baseOnBalls'] + jugador['stats']['pitching']['intentionalWalks'])
-            df['HP'].append(jugador['stats']['pitching']['hitByPitch'])
-            df['atBats'].append(jugador['stats']['pitching']['atBats'])
-            df['runs'].append(jugador['stats']['pitching']['runs'])
-            df['earnedRuns'].append(jugador['stats']['pitching']['earnedRuns'])
-            df['seasonERA'].append(float(jugador['seasonStats']['pitching']['era']))
+    for jugador in data['teams']['home']['players'].values():
+        if jugador['position']['code'] != '1' or len(jugador['stats']['pitching']) == 0:
+            continue
+        df['Team'].append(home)
+        df['Pitcher'].append(jugador['person']['fullName'])
+        df['BattersFaced'].append(jugador['stats']['pitching']['battersFaced'])
+        df['TotalOuts'].append(jugador['stats']['pitching']['outs'])
+        df['Doubles'].append(jugador['stats']['pitching']['doubles'])
+        df['Triples'].append(jugador['stats']['pitching']['triples'])
+        df['HomeRuns'].append(jugador['stats']['pitching']['homeRuns'])
+        df['Singles'].append(jugador['stats']['pitching']['hits'] - df['Doubles'][-1] - df['Triples'][-1] - df['HomeRuns'][-1])
+        df['Walks'].append(jugador['stats']['pitching']['baseOnBalls'] + jugador['stats']['pitching']['intentionalWalks'])
+        df['HP'].append(jugador['stats']['pitching']['hitByPitch'])
+        df['atBats'].append(jugador['stats']['pitching']['atBats'])
+        df['runs'].append(jugador['stats']['pitching']['runs'])
+        df['earnedRuns'].append(jugador['stats']['pitching']['earnedRuns'])
+        df['seasonERA'].append(float(jugador['seasonStats']['pitching']['era']))
 
     df = pd.DataFrame(df)
-    df.to_csv('../data/pitchers.csv', index=False)
+    df.to_csv('data/pitchers.csv', index=False)
+
+def getBatters(rawData):
+    df = {
+        'Team': [],
+        'Bateador': [],
+        'Position': [],
+        'Singles': [],
+        'Doubles': [],
+        'Triples': [],
+        'HomeRuns': [],
+        'Walks': [],
+        'HP': [],
+        'atBats': [],
+        'runs': [],
+        'rbi': [],
+        'seasonAVG': [],
+        'seasonOPS': [],
+    }
+    data = rawData['liveData']['boxscore']
+    away = data['teams']['away']['team']['name']
+    for jugador in data['teams']['away']['players'].values():
+        if len(jugador['stats']['batting']) == 0:
+            continue
+        df['Team'].append(away)
+        df['Bateador'].append(jugador['person']['fullName'])
+        df['Position'].append(jugador['position']['abbreviation'])
+        df['Doubles'].append(jugador['stats']['batting']['doubles'])
+        df['Triples'].append(jugador['stats']['batting']['triples'])
+        df['HomeRuns'].append(jugador['stats']['batting']['homeRuns'])
+        df['Singles'].append(jugador['stats']['batting']['hits'] - df['Doubles'][-1] - df['Triples'][-1] - df['HomeRuns'][-1])
+        df['Walks'].append(jugador['stats']['batting']['baseOnBalls'] + jugador['stats']['batting']['intentionalWalks'])
+        df['HP'].append(jugador['stats']['batting']['hitByPitch'])
+        df['atBats'].append(jugador['stats']['batting']['atBats'])
+        df['runs'].append(jugador['stats']['batting']['runs'])
+        df['rbi'].append(jugador['stats']['batting']['rbi'])
+        df['seasonAVG'].append(float(jugador['seasonStats']['batting']['avg']))
+        df['seasonOPS'].append(float(jugador['seasonStats']['batting']['ops']))
+
+    home = data['teams']['home']['team']['name']
+    for jugador in data['teams']['home']['players'].values():
+        if len(jugador['stats']['batting']) == 0:
+            continue
+        df['Team'].append(home)
+        df['Bateador'].append(jugador['person']['fullName'])
+        df['Position'].append(jugador['position']['abbreviation'])
+        df['Doubles'].append(jugador['stats']['batting']['doubles'])
+        df['Triples'].append(jugador['stats']['batting']['triples'])
+        df['HomeRuns'].append(jugador['stats']['batting']['homeRuns'])
+        df['Singles'].append(jugador['stats']['batting']['hits'] - df['Doubles'][-1] - df['Triples'][-1] - df['HomeRuns'][-1])
+        df['Walks'].append(jugador['stats']['batting']['baseOnBalls'] + jugador['stats']['batting']['intentionalWalks'])
+        df['HP'].append(jugador['stats']['batting']['hitByPitch'])
+        df['atBats'].append(jugador['stats']['batting']['atBats'])
+        df['runs'].append(jugador['stats']['batting']['runs'])
+        df['rbi'].append(jugador['stats']['batting']['rbi'])
+        df['seasonAVG'].append(float(jugador['seasonStats']['batting']['avg']))
+        df['seasonOPS'].append(float(jugador['seasonStats']['batting']['ops']))
+
+    df = pd.DataFrame(df)
+    df.to_csv('data/bateadores.csv', index=False)
+
 
 if __name__ == "__main__":
     juego_id = 809632
     datosJuegoRaw = requests.get(urlBaseV1_1 + f'game/{juego_id}/feed/live').content
     datosJuegoRaw = json.loads(datosJuegoRaw)
     getPitchers(datosJuegoRaw)
+    getBatters(datosJuegoRaw)
